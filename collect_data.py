5285cffd22aa29432b735d1ef86d85943fa6bee1
Spencer Weishaar <sbweishaar@icloud.com>
2025-10-27 00:28:40 -0500

    deleted old code

--- collect_data.py
+++ collect_data.py
@@ -56,7 +56,6 @@ class Collect_game_and_player_data:
 
         return players_df_test
     
-        players_df_test.to_csv('nba_player_averages_2025.csv', index=False)
             
 
     def get_player_data(self):
@@ -82,95 +81,6 @@ class Collect_game_and_player_data:
 
         return test_players_df, train_players_df, test_games_df, train_games_df, test_boxscores_df, train_boxscores_df
 
-
-
-
-
-        # Get test data and store to a csv
-
-        all_player_train_data = []
-
-        # Loop through seasons from 2000 to 2024 for test data and store to csv
-        # This data will be used to fit the min-max scaler, because it contains all the test player data.
-        for season in range(2000, 2025):
-
-            print(f"In season: {season}")
-        
-            player_data = self.collect_data(season)
-            all_player_train_data.extend(player_data)
-
-
-        players_df_train = pd.concat(all_player_train_data, ignore_index=True)
-        players_df_train.to_csv('nba_player_averages_2000-2024.csv', index=False)
-
-
-        #################################################################################################################################################################
-
-        # Loop through season 2025 for test data and store to csv
-
-        # This data might be used for the test data, but idk if it will be used yet, because I might get the data another way. 
-        all_player_test_data = []
-        for season in range(2025, 2026):
-            print(f"In season: {season}")
-            player_data = self.collect_data(season)
-            all_player_test_data.extend(player_data)
-
-        players_df_test = pd.concat(all_player_test_data, ignore_index=True)
-        players_df_test.to_csv('nba_player_averages_2025.csv', index=False)
-
-
-
-        #################################################################################################################################################################
-
-
-        all_games_test_data = []
-
-        for season in range(2000, 2025):
-
-            print(f"Collecting game logs for season: {season}")
-
-            try:
-                season_str = f"{season}-{str(season + 1)[-2:]}"
-                game_log = leaguegamelog.LeagueGameLog(season=season_str)
-                games_df = game_log.get_data_frames()[0]
-                all_games_test_data.append(games_df)
-            except Exception as e:
-                print(f"Error retrieving game logs for season {season_str}: {e}")
-                time.sleep(1)  # wait before retrying or moving on
-                continue
-        
-        games_df_test = pd.concat(all_games_test_data, ignore_index=True)
-        games_df_test.to_csv('nba_game_logs_2000-2024.csv', index=False)
-
-
-
-        ##################################################################################################################################################################
-
-
-
-
-        all_games_test_data = []
-
-        for season in range(2025, 2026):
-
-            print(f"Collecting game logs for season: {season}")
-
-            try:
-                season_str = f"{season}-{str(season + 1)[-2:]}"
-                game_log = leaguegamelog.LeagueGameLog(season=season_str)
-                games_df = game_log.get_data_frames()[0]
-                all_games_test_data.append(games_df)
-            except Exception as e:
-                print(f"Error retrieving game logs for season {season_str}: {e}")
-                time.sleep(1)  # wait before retrying or moving on
-                continue
-        
-        games_df_test = pd.concat(all_games_test_data, ignore_index=True)
-        games_df_test.to_csv('nba_game_logs_2025.csv', index=False)
-
-
-
-        return players_df_train, players_df_test
 
 
 
